645dfc9238da6dc528ff23a8252f57c1c64081eb
Rajiv Kumar <rajiv.kumar@nectechnologies.in>
2017-01-19 13:48:27 +0530

    Switch to oslo_log
    Change-Id: Id9ce71a53f4392ea1bd408681d01b6174d86ae61

--- patrole_tempest_plugin/rbac_rule_validation.py
+++ patrole_tempest_plugin/rbac_rule_validation.py
@@ -13,8 +13,7 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-import logging
-
+from oslo_log import log as logging
 from tempest import config
 from tempest.lib import exceptions
 
