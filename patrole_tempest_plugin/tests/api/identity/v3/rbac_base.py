9d0d7d63ba9837a9ef33fd4037725bd038fb412f
Nicolas Helgeson <nh202b@att.com>
2017-01-25 13:27:00 -0800

    Identity V3 tests -  Users
    Tests for identity v3 users
    
    Partially-Implements bp: initial-tests-identity
    Change-Id: I03a38115bb2fc9dcdebd83c55e429e5e8a1211b5
    Co-Authored-By: Cliff Parsons <cp769u@att.com>
    Co-Authored-By: Hemachandra Reddy <hr858f@att.com>
    Co-Authored-By: Nishant Kumar <nk776q@att.com>

---
+++ patrole_tempest_plugin/tests/api/identity/v3/rbac_base.py
@@ -0,0 +1,40 @@
+# Copyright 2017 AT&T Corporation.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from tempest.api.identity import base
+from tempest import config
+
+CONF = config.CONF
+
+
+class BaseIdentityV3RbacAdminTest(base.BaseIdentityV3AdminTest):
+
+    credentials = ['primary', 'admin']
+
+    @classmethod
+    def skip_checks(cls):
+        super(BaseIdentityV3RbacAdminTest, cls).skip_checks()
+        if not CONF.rbac.rbac_flag:
+            raise cls.skipException(
+                "%s skipped as RBAC Flag not enabled" % cls.__name__)
+        if CONF.auth.tempest_roles != ['admin']:
+            raise cls.skipException(
+                "%s skipped because tempest roles is not admin" % cls.__name__)
+
+    @classmethod
+    def resource_setup(cls):
+        super(BaseIdentityV3RbacAdminTest, cls).resource_setup()
+        cls.auth_provider = cls.os.auth_provider
+        cls.admin_client = cls.os_adm.identity_v3_client
