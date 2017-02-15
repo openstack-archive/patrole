cf937f14d897e162ee535fbe75d84c78a1bf8235
Felipe Monteiro <felipe.monteiro@att.com>
2017-02-15 12:27:06 -0500

    Compute API Floating Ips Tests.
    Adds tests for compute floating ips API.
    
    Since this API is deprecated for microversions
    beginning at 2.36, the microversion for this test is explicitly
    set to 2.35, 2.36 onward will throw a 404, resulting in
    confusion, because the Patrole framework will interpret this
    as a policy exception, not necessarily a microversioning
    issue [1].
    
    This patch also breaks with previous Patrole convention by
    altering the way the skip_checks classmethod is written, to
    align with the way Tempest compute skip_checks are currently
    written; as an example see [2].
    
    [1] https://developer.openstack.org/api-ref/compute/?expanded=list-floating-ip-addresses-detail
    [2] https://github.com/openstack/tempest/blob/master/tempest/api/compute/admin/test_flavors_access.py
    
    Co-Authored-By: Chi Lo <cl566n@att.com>
    
    Change-Id: I3cb8418b89cf8d7e7d549bd5fc7d54cffac6af2f
    Partially-Implements: blueprint initial-tests-compute

---
+++ patrole_tempest_plugin/tests/api/compute/test_floating_ips_rbac.py
@@ -0,0 +1,57 @@
+#    Copyright 2017 AT&T Corporation.
+#    All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from tempest import config
+from tempest.lib import decorators
+from tempest import test
+
+from patrole_tempest_plugin import rbac_rule_validation
+from patrole_tempest_plugin.rbac_utils import rbac_utils
+from patrole_tempest_plugin.tests.api.compute import rbac_base
+
+CONF = config.CONF
+
+
+class FloatingIpsRbacTest(rbac_base.BaseV2ComputeRbacTest):
+
+    # Tests will fail with a 404 starting from microversion 2.36, according to:
+    # https://developer.openstack.org/api-ref/
+    # compute/?expanded=list-floating-ip-addresses-detail
+    max_microversion = '2.35'
+
+    @classmethod
+    def setup_clients(cls):
+        super(FloatingIpsRbacTest, cls).setup_clients()
+        cls.client = cls.floating_ips_client
+
+    @classmethod
+    def skip_checks(cls):
+        super(FloatingIpsRbacTest, cls).skip_checks()
+        if not test.is_extension_enabled('os-floating-ips', 'compute'):
+            msg = "%s skipped as os-floating-ips extension not enabled." \
+                  % cls.__name__
+            raise cls.skipException(msg)
+
+    def tearDown(self):
+        rbac_utils.switch_role(self, switchToRbacRole=False)
+        super(FloatingIpsRbacTest, self).tearDown()
+
+    @decorators.idempotent_id('ac1b3053-f755-4cda-85a0-30e88b88d7ba')
+    @rbac_rule_validation.action(
+        service="nova",
+        rule="os_compute_api:os-floating-ips")
+    def test_list_floating_ips(self):
+        rbac_utils.switch_role(self, switchToRbacRole=True)
+        self.client.list_floating_ips()['floating_ips']
