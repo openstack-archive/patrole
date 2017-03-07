6448b4a2cdd6a84a03134d21948d64816371b632
Felipe Monteiro <felipe.monteiro@att.com>
2017-03-07 14:13:14 -0500

    Add pip install patrole command to pre_test_hook.sh.
    Add sudo "pip install -e ." inside patrole directory
    to install the plugin so that Tempest automatically
    detects it.
    
    Change-Id: I6ca2c5dcf1f3e265ea0abd5f5f1f9e89b01e98f1

--- contrib/pre_test_hook.sh
+++ contrib/pre_test_hook.sh
@@ -12,28 +12,8 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
-# This script is executed inside post_test_hook function in devstack gate.
-# First argument ($1) expects 'rbac-role' as value for setting appropriate
-# tempest rbac option 'rbac_test_role'.
+# This script is executed inside pre_test_hook function in devstack gate.
+# Installs patrole tempest plugin manually.
 
-sudo chown -R jenkins:stack $BASE/new/tempest
-sudo chown -R jenkins:stack $BASE/data/tempest
-
-# Import devstack function 'iniset'
-source $BASE/new/devstack/functions
-
-export TEMPEST_CONFIG=${TEMPEST_CONFIG:-$BASE/new/tempest/etc/tempest.conf}
-
-# First argument is expected to contain value equal either to 'admin' or
-# 'member' (both lower-case).
-RBAC_ROLE=$1
-
-if [[ "$RBAC_ROLE" == "member" ]]; then
-    $RBAC_ROLE = "Member"
-fi
-
-# Set rbac_flag=True under [rbac] section in tempest.conf
-iniset $TEMPEST_CONFIG rbac rbac_flag True
-
-# Set rbac_test_role=$RBAC_ROLE under [rbac] section in tempest.conf
-iniset $TEMPEST_CONFIG rbac rbac_test_role $RBAC_ROLE
+PATROLE_DIR=$BASE/new/patrole
+sudo pip install -e $PATROLE_DIR
