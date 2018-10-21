cacbd211250d355bcbb2bbf049cfb4bd8c94ee04
Felipe Monteiro <felipe.monteiro@att.com>
2018-10-21 12:30:17 -0400

    fix: Rename test classes causing accidental skip
    This patch set renames two classes that are neutron plugin
    rbac tests but don't end in the appropriate suffix of
    "PluginRbacTest" which is used by .zuul.yaml to correctly execute
    the tests [0].
    
    This means that, in effect, these tests aren't being run in any
    jobs. This patch set resolves the issue.
    
    In a follow up patch set a hacking rule will be added to enforce
    correct naming for plugin rbac tests.
    
    [0] https://github.com/openstack/patrole/blob/0c7f83ea1716462737a40cc32f27b95abf4557af/.zuul.yaml#L168
    
    Change-Id: Ieac5ebf70eefcebf1aebc26da95d58fcd68a202e

--- patrole_tempest_plugin/tests/api/network/test_qos_rbac.py
+++ patrole_tempest_plugin/tests/api/network/test_qos_rbac.py
@@ -22,18 +22,18 @@ from patrole_tempest_plugin import rbac_rule_validation
 from patrole_tempest_plugin.tests.api.network import rbac_base as base
 
 
-class QosRbacTest(base.BaseNetworkPluginRbacTest):
+class QosPluginRbacTest(base.BaseNetworkPluginRbacTest):
 
     @classmethod
     def skip_checks(cls):
-        super(QosRbacTest, cls).skip_checks()
+        super(QosPluginRbacTest, cls).skip_checks()
         if not utils.is_extension_enabled('qos', 'network'):
             msg = "qos extension not enabled."
             raise cls.skipException(msg)
 
     @classmethod
     def resource_setup(cls):
-        super(QosRbacTest, cls).resource_setup()
+        super(QosPluginRbacTest, cls).resource_setup()
         cls.network = cls.create_network()
 
     def create_policy(self, name=None):
