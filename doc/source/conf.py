d69a6367a1340c4828a6e5eab68211bba5f2ae73
Andreas Jaeger <aj@suse.com>
2020-04-21 07:39:05 +0200

    Cleanup py27 support
    This repo is now testing only with Python 3, so let's make
    a few cleanups:
    - Remove python 2.7 stanza from setup.py
    - Remove obsolete sections from setup.cfg
    - Update classifiers
    - Use newer openstackdocstheme and Sphinx versions
    - Cleanup */source/conf.py to remove now obsolete content.
    - Remove install_command from tox.ini, the default is fine
    - Remove hacking requirements from lower-constraints, they
      are not needed for install
    
    Change-Id: I150a5ee2cd08abf5ce9cf9daf2835007dea0dffd

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -89,9 +89,6 @@ repository_name = 'openstack/patrole'
 bug_project = 'patrole'
 bug_tag = ''
 
-# Must set this variable to include year, month, day, hours, and minutes.
-html_last_updated_fmt = '%Y-%m-%d %H:%M'
-
 # Output file base name for HTML help builder.
 htmlhelp_basename = 'patroledoc'
 
